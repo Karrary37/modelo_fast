cc5300e868b27ad5f230d6ecfe3cda627cec3038
Guilherme Beserra Lisboa <guilherme.lisboa@grupobyx.com.br>
2024-05-25 13:37:08 -0300

    adicionando banco de dados e alguns ajustes

---
+++ app/domain/repositories/sqlalchemy_link_repository.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from typing import Optional
+from app.domain.models.link import LinkModel
+from app.domain.repositories.link_repository import LinkRepository
+
+class SQLAlchemyLinkRepository(LinkRepository):
+    def __init__(self, db: Session):
+        self.db = db
+
+    def save_link(self, link: LinkModel) -> LinkModel:
+        db_link = LinkModel(id=link.id, original_url=str(link.original_url), shortened_url=link.shortened_url)
+        self.db.add(db_link)
+        self.db.commit()
+        self.db.refresh(db_link)
+        return db_link
+
+    def get_link_by_shortened_url(self, shortened_url: str) -> Optional[LinkModel]:
+        return self.db.query(LinkModel).filter(LinkModel.shortened_url == shortened_url).first()
