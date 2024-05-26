820be9f5c678523ecbce4171c2cbb8e332c84741
Nattan Karrary <74218114+Karrary37@users.noreply.github.com>
2024-05-26 16:02:46 -0300

    Test login

--- app/domain/repositories/dynamodb_link_repository.py
+++ app/domain/repositories/dynamodb_link_repository.py
@@ -58,7 +58,7 @@ async def create_dynamodb_table():
             TableName=table_name,
             KeySchema=[{'AttributeName': 'id', 'KeyType': 'HASH'}],
             AttributeDefinitions=[{'AttributeName': 'id', 'AttributeType': 'S'}],
-            ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5},
+            ProvisionedThroughput={'ReadCapacityUnits': 100, 'WriteCapacityUnits': 100},
         )
         table.meta.client.get_waiter('table_exists').wait(TableName=table_name)
         print(f'Table {table.table_name} created successfully.')
---
+++ auth/test/test_verify_jwt_token.py
@@ -0,0 +1,43 @@
+import jwt
+import pytest
+from fastapi import HTTPException, status
+from auth.domain.models.schemas_auth import TokenData
+from auth.domain.repositories.verify_jwt import verify_jwt_token
+from config import settings
+
+
+def test_verify_jwt_token_valid():
+    username = "test_user"
+    token = jwt.encode({'sub': username}, settings.APP_NAME, algorithm='HS256')
+
+    token_data = verify_jwt_token(token)
+
+    assert isinstance(token_data, TokenData)
+    assert token_data.username == username
+
+
+def test_verify_jwt_token_expired():
+    token = jwt.encode({'sub': 'expired_user', 'exp': 0}, settings.APP_NAME, algorithm='HS256')
+
+    with pytest.raises(HTTPException) as e:
+        verify_jwt_token(token)
+    assert e.value.status_code == status.HTTP_401_UNAUTHORIZED
+    assert e.value.detail == 'Token expired'
+
+
+def test_verify_jwt_token_invalid():
+    token = "invalid_token"
+
+    with pytest.raises(HTTPException) as e:
+        verify_jwt_token(token)
+    assert e.value.status_code == status.HTTP_401_UNAUTHORIZED
+    assert e.value.detail == 'Invalid token'
+
+
+def test_verify_jwt_token_missing_username():
+    token = jwt.encode({}, settings.APP_NAME, algorithm='HS256')
+
+    with pytest.raises(HTTPException) as e:
+        verify_jwt_token(token)
+    assert e.value.status_code == status.HTTP_401_UNAUTHORIZED
+    assert e.value.detail == 'Invalid token'
