0721edf1c52702bb49a419afa21ef79bf1a265ad
Guilherme Beserra Lisboa <guilherme.lisboa@grupobyx.com.br>
2024-05-26 15:45:34 -0300

    unit teste

--- test/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Adicione o diretório do projeto ao caminho do Python
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
--- test/test_api.py
+++ test/test_api.py
@@ -1,22 +1,65 @@
 import pytest
+from httpx import AsyncClient
+from fastapi import FastAPI
 from app.adapters.api.api_shorten import app as shorten_app
-from fastapi.testclient import TestClient
+import boto3
+from botocore.exceptions import ClientError
 
 @pytest.fixture
-def client():
-    with TestClient(shorten_app) as client:
-        yield client
+def app() -> FastAPI:
+    return shorten_app
 
-def test_create_link(client):
-    response = client.post('/shorten/', json={"original_url": "https://example.com"})
+@pytest.fixture
+async def async_client(app: FastAPI) -> AsyncClient:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
+
+@pytest.fixture(scope='session')
+def dynamodb_client():
+    return boto3.client('dynamodb', region_name='us-east-1')
+
+@pytest.fixture(scope='session')
+def dynamodb_resource():
+    return boto3.resource('dynamodb', region_name='us-east-1')
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_and_teardown_dynamodb_table(dynamodb_resource, dynamodb_client):
+    table_name = 'links'
+
+    # Create the table
+    table = dynamodb_resource.create_table(
+        TableName=table_name,
+        KeySchema=[{'AttributeName': 'id', 'KeyType': 'HASH'}],
+        AttributeDefinitions=[{'AttributeName': 'id', 'AttributeType': 'S'}],
+        ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5},
+    )
+    table.meta.client.get_waiter('table_exists').wait(TableName=table_name)
+
+    yield
+
+    # Delete the table
+    table.delete()
+    table.meta.client.get_waiter('table_not_exists').wait(TableName=table_name)
+
+@pytest.mark.asyncio
+async def test_create_link(async_client: AsyncClient):
+    response = await async_client.post(
+        "/shorten/",
+        json={"original_url": "http://example.com"}
+    )
     assert response.status_code == 200
-    assert 'url_encurtado' in response.json()
+    assert "shortened_url" in response.json()
 
-def test_redirect_to_original(client):
-    response = client.post('/shorten/', json={"original_url": "https://example.com"})
+@pytest.mark.asyncio
+async def test_redirect_to_original(async_client: AsyncClient):
+    # Primeiro, crie um link encurtado
+    response = await async_client.post(
+        "/shorten/",
+        json={"original_url": "http://example.com"}
+    )
+    shortened_url = response.json()["shortened_url"].split("/")[-1]
+
+    # Teste a redireção
+    response = await async_client.get(f"/{shortened_url}/")
     assert response.status_code == 200
-    shortened_url = response.json()['url_encurtado'].split('/')[-1]
-
-    redirect_response = client.get(f'/{shortened_url}/', allow_redirects=False)
-    assert redirect_response.status_code == 307
-    assert redirect_response.headers['location'] == 'https://example.com'
+    assert response.headers["location"] == "http://example.com"
